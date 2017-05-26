1b8c31efae1c59688cbecc93e0c481542feb2e16
white <white@WHITEBOOK>
2017-05-26 01:42:43 -0300

    Criado pacote da interface gráfica e classe Produto

---
+++ OndeComprar/src/Produto.java
@@ -0,0 +1,34 @@
+
+public class Produto {
+	
+	private int id;
+	private String nome;
+	private String marca;
+	private float preco;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public float getPreco() {
+		return preco;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+}
